96cd2150d9082a6f06a00f5fb4e5ce6c7a49b3de
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-01-21 11:02:23 -0300

    Calculos adcionados

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -3,6 +3,8 @@ import React, {useState} from "react"
 
 function Calculadora() {
     const [calculo, setCalculo] = useState(0)
+    const [numeroAntigo, setNumeroAntigo] = useState(0)
+    const [operador, setOperador] = useState()
 
     function inputNumero(event) {
         let numeroClicado = event.target.value
@@ -16,8 +18,40 @@ function Calculadora() {
     function limpar() {
         setCalculo(0)
     }
+    function porcentagem() {
+        setCalculo(calculo/100)
+    }
+    function mudarSinal() {
+        if(calculo > 0){
+            setCalculo(-calculo)
+        }else if(calculo < 0){
+            setCalculo(Math.abs(calculo))
+        }
+    }
+    function pegarOperador(event) {
+        let inputOperador = event.target.value
+        setOperador(inputOperador)
+        setNumeroAntigo(calculo)
+        setCalculo(0)
+    }
+    function calcular() {
+        if(operador === "/"){
+            setCalculo(parseFloat(numeroAntigo) / parseFloat(calculo))
+        }
+        else if(operador === "+"){
+            setCalculo(parseFloat(numeroAntigo) + parseFloat(calculo))
+        }
+        else if(operador === "-"){
+            setCalculo(parseFloat(numeroAntigo) - parseFloat(calculo))
+        }
+        else if(operador === "X"){
+            setCalculo(parseFloat(numeroAntigo) * parseFloat(calculo))
+        }
+    }
 
     return(
+        <>
+        <h1 id="titulo-principal">Calculadora</h1>
         <div className="container-principal">
             <div className="resultado">
                 <h1>{calculo}</h1>
@@ -26,35 +60,36 @@ function Calculadora() {
             <div className="grupo-botoes">
                 <div className="grupo-4">
                     <button onClick={limpar} className="branco">AC</button>
-                    <button onClick={inputNumero} className="branco">+/-</button>
-                    <button onClick={inputNumero} className="branco">%</button>
-                    <button onClick={inputNumero} className="laranja">/</button>
+                    <button onClick={mudarSinal} className="branco">+/-</button>
+                    <button onClick={porcentagem} className="branco">%</button>
+                    <button onClick={pegarOperador} className="laranja" value={"/"}>/</button>
                 </div>
                 <div className="grupo-4">
                     <button onClick={inputNumero} value={7}>7</button>
                     <button onClick={inputNumero} value={8}>8</button>
                     <button onClick={inputNumero} value={9}>9</button>
-                    <button onClick={inputNumero} className="laranja">X</button>
+                    <button onClick={pegarOperador} className="laranja" value={"X"}>X</button>
                 </div>
                 <div className="grupo-4">
                     <button onClick={inputNumero} value={4}>4</button>
                     <button onClick={inputNumero} value={5}>5</button>
                     <button onClick={inputNumero} value={6}>6</button>
-                    <button onClick={inputNumero} className="laranja">-</button>
+                    <button onClick={pegarOperador} className="laranja" value={"-"}>-</button>
                 </div>
                 <div className="grupo-4">
                     <button onClick={inputNumero} value={1}>1</button>
                     <button onClick={inputNumero} value={2}>2</button>
                     <button onClick={inputNumero} value={3}>3</button>
-                    <button onClick={inputNumero} className="laranja">+</button>
+                    <button onClick={pegarOperador} className="laranja" value={"+"}>+</button>
                 </div>
                 <div className="grupo-4">
                     <button onClick={inputNumero} value={0} className="zero">0</button>
-                    <button onClick={inputNumero}>,</button>
-                    <button onClick={inputNumero} className="branco">=</button>
+                    <button onClick={inputNumero} value={"."}>,</button>
+                    <button onClick={calcular} className="branco">=</button>
                 </div>
             </div>
         </div>
+        </>
     )
 }
 
